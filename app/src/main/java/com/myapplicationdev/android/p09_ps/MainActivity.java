52c295fb132c8652e4fd64f5a468ada0338c6571
19022187 <19022187@myrp.edu.sg>
2021-07-15 19:41:19 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p09_ps/MainActivity.java
@@ -0,0 +1,151 @@
+package com.myapplicationdev.android.p09_ps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+import androidx.fragment.app.FragmentManager;
+
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGetLocationUpdate, btnRemoveLocationUpdate, btnCheckRecords;
+    TextView tv;
+    FusedLocationProviderClient client;
+    LocationCallback mLocationCallBack;
+    LocationRequest mLocationRequest;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                fm.findFragmentById(R.id.map);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationRequest = new LocationRequest();
+        mLocationCallBack = new LocationCallback();
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+
+
+
+        mapFragment.getMapAsync(new OnMapReadyCallback(){
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                UiSettings ui = map.getUiSettings();
+                ui.setCompassEnabled(true);
+                ui.setZoomControlsEnabled(true);
+
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                } else {
+                    Log.e("GMap - Permission", "GPS access has not been granted");
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                }
+            }
+        });
+
+        final LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setSmallestDisplacement(100);
+
+        mLocationCallBack = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                if (locationResult != null){
+                    Location locData = locationResult.getLastLocation();
+                    String msg = "NLast known location \n" +
+                            "Lat: " + locData.getLatitude() + "\n" +
+                            "Lng: " + locData.getLongitude();
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    tv.setText(msg);
+                }
+            }
+        };
+
+
+        btnGetLocationUpdate = findViewById(R.id.btnGetLocationUpdate);
+        btnRemoveLocationUpdate = findViewById(R.id.btnRemoveLocationUpdate);
+        btnCheckRecords = findViewById(R.id.btnCheckRecords);
+
+        btnGetLocationUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermission() == true){
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallBack, null);
+                }
+            }
+        });
+
+        btnRemoveLocationUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermission() == true) {
+                    client.removeLocationUpdates(mLocationCallBack);
+                }
+            }
+        });
+
+        btnCheckRecords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private boolean checkPermission(){
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
